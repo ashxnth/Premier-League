349681509c11b5ce1693a2888ceff00231eafd86
ashanth <aranjith@ryerson.ca>
2021-05-22 22:20:48 -0400

    Create TradingView Widgets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { HStack } from "@chakra-ui/react"
 import Link from 'next/link'
+import { Heading, Flex, Text, Input, Spacer, Box } from "@chakra-ui/react"
 
 export default function Navbar() {
-    return(
-        <HStack spacing={8} justify={["flex-start", "flex-start"]}>
-            <Link href="/">Nasdash</Link>
-            <Link href="/market-watch">MarketWatch</Link>
-        </HStack>
-    )
+    return (
+        <Flex bg="teal.500" color="white" justify="space-between" padding={4} align="center" position="sticky">
+            <Heading as="h1" size="lg" letterSpacing={"tighter"} mr={4}>
+                <Link href="/">Nasdash</Link>
+            </Heading>
+            <Text><Link href="/market-watch">MarketWatch</Link></Text>
+            <Spacer />
+            <Box>
+                <Input variant="outline" color="black" bg="white" placeholder="Search" />
+            </Box>
+        </Flex>
+    );
 }
